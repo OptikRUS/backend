929862693238bce8bca5a99f8c03ea586c8b377d
optikrus <optikrus61@gmail.com>
2022-02-19 20:11:56 +0300

    add users model with migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField('email address', blank=True, unique=True)
+
